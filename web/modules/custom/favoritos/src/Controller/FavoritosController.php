f3ea40f44dffe02136913bdf7e6f158e2e6bccab
Fernando Bernal Belmonte <bernal@MacBook-Air-de-Fer.local>
2026-04-04 12:11:39 +0200

    dejar todo limpio y ordenado y recolocar carpetas

--- web/modules/custom/favoritos/src/Controller/FavoritosController.php
+++ web/modules/custom/favoritos/src/Controller/FavoritosController.php
@@ -2,39 +2,23 @@
 
 namespace Drupal\favoritos\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Drupal\node\Entity\Node;
-use Drupal\user\Entity\User;
 
-class FavoritosController extends ControllerBase {
+class FavoritosController {
 
-  public function toggle($nid, Request $request) {
+  public function toggle($node) {
 
-    $user = User::load(\Drupal::currentUser()->id());
-    $node = Node::load($nid);
+    $node = Node::load($node);
 
-    if (!$user || !$node) {
-      return new JsonResponse(['estado' => 'error']);
+    if (!$node) {
+      return new JsonResponse(['error' => 'Nodo no encontrado'], 404);
     }
 
-    $favoritos = $user->get('field_favoritos')->getValue();
-    $ids = array_column($favoritos, 'target_id');
-
-    if (in_array($nid, $ids)) {
-      $ids = array_diff($ids, [$nid]);
-    } else {
-      $ids[] = $nid;
-    }
-
-    $user->set('field_favoritos', array_map(function ($id) {
-      return ['target_id' => $id];
-    }, $ids));
-
-    $user->save();
-
-    return new JsonResponse(['estado' => 'ok']);
+    return new JsonResponse([
+      'estado' => 'ok',
+      'node' => $node->id(),
+    ]);
   }
 
 }
